8a1cd504956681b5c0c95289bf2a81c4c28841e4
timetecinc <rayg@ttmemory.com>
2019-10-02 15:37:14 -0700

    updated the chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -8,44 +8,81 @@ export default class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: 2,
-      messages: [
-        { fromMe: true, text: "hello", id: 0 },
-        { fromMe: false, text: "hello", id: 1 }
-      ],
-      inputValue: ""
+      loginStatus: false,
+      userName: "",
+      messages: [],
+      inputValue: "",
+      isInputVaild: false
     };
     this.messageRef = firebase
       .database()
       .ref()
-      .child("message");
+      .child("users");
   }
 
   handleOnClick = () => {
-    let id = this.state.id;
     if (this.state.inputValue !== "") {
-      let newMessageList = this.state.messages;
       let newMessage = {
-        fromMe: true,
-        text: this.state.inputValue,
-        id: id++
+        senderName: this.state.userName,
+        text: this.state.inputValue
       };
-      newMessageList.push(newMessage);
+
       this.setState({
-        messages: newMessageList,
-        inputValue: "",
-        id: id++
+        inputValue: ""
       });
-      this.messageRef.push(newMessage);
+      this.messageRef.child(this.state.userName).push(newMessage);
     }
   };
   handleInputChange = e => {
-    this.setState({ inputValue: e.target.value });
+    let invalidCharList = [".", "$", "#", "[", "]", "/"];
+    for (let i = 0; i < invalidCharList.length; i++) {
+      //console.log("input", e.target.value);
+      //console.log("input", char);
+      if (e.target.value.includes(invalidCharList[i])) {
+        console.log("input invalid");
+        this.setState({ isInputVaild: false });
+        return;
+      }
+    }
+
+    this.setState({ isInputVaild: true, inputValue: e.target.value });
   };
   handleKeyPress = e => {
     if (e.key !== "Enter") return;
     this.handleOnClick();
   };
+  handleSignIn = e => {
+    //when the input name is not empty
+    if (this.state.inputValue.length > 0) {
+      console.log("customer name", this.state.inputValue);
+      //this.listenMessages();
+
+      this.messageRef.child(this.state.inputValue).on("value", message => {
+        //console.log(Object.values(message.val()));
+        if (message.val()) {
+          this.setState({ messages: Object.values(message.val()) });
+        }
+      });
+      this.setState({
+        isInputVaild: true,
+        loginStatus: true,
+        inputValue: "",
+        userName: this.state.inputValue
+      });
+    }
+  };
+  listenMessages() {
+    let userName = this.state.userName;
+
+    console.log("user name is ", userName);
+    this.messageRef.child("ray").on("value", message => {
+      console.log(Object.values(message.val()));
+    });
+  }
+  componentWillUnmount() {
+    this.messageRef.remove();
+  }
+
   render() {
     return (
       <div>
@@ -53,58 +90,17 @@ export default class Chat extends React.Component {
         <div style={{ backgroundColor: "black", height: "40px" }}>
           <h3 style={{ color: "white" }}>Contact Me</h3>
         </div>
-        <div
-          className="messageOutterFlexContainer"
-          style={{
-            borderStyle: "solid",
-            borderWidth: " 1px 1px 0px 1px",
-            height: "500px",
-            overflow: "auto"
-          }}
-        >
-          <div
-            className="messageFlexContainer"
-            style={{ display: "flex", flexDirection: "column" }}
-          >
-            {this.state.messages.map(message => {
-              return (
-                <Messages
-                  key={message.id}
-                  fromMe={message.fromMe}
-                  text={message.text}
-                />
-              );
-            })}
-          </div>
-        </div>
-        <div
-          className="input-group mb-3"
-          style={{
-            width: "100% !important",
-            alignItems: "stretch !important",
-            flexWrap: "wrap",
-            display: "flex !important"
-          }}
-        >
-          <input
-            type="text"
-            className="from-control"
-            style={{ flex: "1 1 auto" }}
-            value={this.state.inputValue}
-            onChange={this.handleInputChange}
-            onKeyPress={this.handleKeyPress}
-          />
-          <div className="input-group-append">
-            <button
-              className="btn btn-outline-secondary"
-              type="button"
-              style={{ borderRadius: "0" }}
-              onClick={this.handleOnClick}
-            >
-              Send
-            </button>
-          </div>
-        </div>
+        <ChatWindow
+          isInputVaild={this.state.isInputVaild}
+          loginStatus={this.state.loginStatus}
+          userName={this.state.userName}
+          messages={this.state.messages}
+          inputValue={this.state.inputValue}
+          handleInputChange={this.handleInputChange}
+          handleKeyPress={this.handleKeyPress}
+          handleOnClick={this.handleOnClick}
+          handleSignIn={this.handleSignIn}
+        ></ChatWindow>
       </div>
     );
   }
@@ -116,33 +112,171 @@ class Messages extends React.Component {
       flexDirection: "column"
     };
     let messageTextStyle = {
-      height: "30px",
       borderStyle: "solid",
+      borderWidth: 0,
       borderRadius: "15px",
-      color: "white"
+      color: "black",
+      maxWidth: "70%"
     };
-    if (this.props.fromMe) {
+    if (this.props.userName === this.props.senderName) {
       messageTextStyle.backgroundColor = "cornflowerblue";
+      messageTextStyle.color = "white";
       messageBodyStyle.alignItems = "flex-end";
       messageBodyStyle.marginRight = "10px";
     } else {
-      messageTextStyle.backgroundColor = "orangered";
+      messageTextStyle.backgroundColor = "#e9e9eb";
       messageBodyStyle.alignItems = "flex-start";
       messageBodyStyle.marginLeft = "10px";
     }
-
+    let newText = this.props.text.split("\n").map((item, i) => {
+      return (
+        <p style={{ marginBottom: 0 }} key={i}>
+          {item}
+        </p>
+      );
+    });
     return (
       <div style={messageBodyStyle}>
-        <div className="userName">Users</div>
+        <div className="userName">{this.props.senderName}</div>
         <div style={messageTextStyle}>
           <div
             className="text"
             style={{ marginLeft: "10px", marginRight: "10px" }}
           >
-            {this.props.text}
+            {newText}
           </div>
         </div>
       </div>
     );
+  }
+}
+class ChatWindow extends React.Component {
+  render() {
+    if (this.props.loginStatus === true) {
+      return (
+        <div>
+          <div
+            className="messageOutterFlexContainer"
+            style={{
+              borderStyle: "solid",
+              borderWidth: " 1px 1px 0px 1px",
+              height: "500px",
+              overflow: "auto"
+            }}
+          >
+            <div
+              class="alert alert-warning alert-dismissible fade show"
+              role="alert"
+            >
+              <strong>Hello!</strong> this is a real time one to one chat app.
+              You can messmage me and I will reply you if I see the message now.
+              Thanks.
+              <button
+                type="button"
+                class="close"
+                data-dismiss="alert"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div
+              className="messageFlexContainer"
+              style={{ display: "flex", flexDirection: "column" }}
+            >
+              {this.props.messages.map((message, i) => {
+                return (
+                  <Messages
+                    key={i}
+                    senderName={message.senderName}
+                    text={message.text}
+                    userName={this.props.userName}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <div
+            className="input-group mb-3"
+            style={{
+              width: "100% !important",
+              alignItems: "stretch !important",
+              flexWrap: "wrap",
+              display: "flex !important"
+            }}
+          >
+            <input
+              type="text"
+              className="from-control"
+              style={{ flex: "1 1 auto" }}
+              value={this.props.inputValue}
+              onChange={this.props.handleInputChange}
+              onKeyPress={this.props.handleKeyPress}
+            />
+            <div className="input-group-append">
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                style={{ borderRadius: "0" }}
+                onClick={this.props.handleOnClick}
+              >
+                Send
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      let button;
+      if (this.props.isInputVaild) {
+        button = (
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            style={{ borderRadius: "0" }}
+            onClick={this.props.handleSignIn}
+          >
+            Sign In
+          </button>
+        );
+      } else {
+        button = (
+          <button
+            className="btn btn-outline-danger"
+            type="button"
+            style={{ borderRadius: "0" }}
+            disabled
+          >
+            Please enter a valid name
+          </button>
+        );
+      }
+      return (
+        <div>
+          <h3>Please Enter a name</h3>
+          <div
+            className="input-group mb-3"
+            style={{
+              alignItems: "stretch !important",
+              flexWrap: "wrap",
+              display: "flex !important"
+            }}
+          >
+            <input
+              type="text"
+              className="from-control"
+              value={this.props.inputValue}
+              onChange={this.props.handleInputChange}
+              onKeyPress={this.props.handleKeyPress}
+            />
+            <div className="input-group-append">{button}</div>
+          </div>
+          <p style={{ color: "red" }}>
+            Note:Name cannot contain., $, #, [, ], /, or ASCII control
+            characters 0-31 or 127.
+          </p>
+        </div>
+      );
+    }
   }
 }
--- src/pages/smallComponents.jsx
+++ src/pages/smallComponents.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import ToDoArea from "../components/TodoList";
+//import ToDoArea from "../components/TodoList";
 import Chat from "../components/Chat";
 export default class SmallComponents extends Component {
   render() {
     return (
       <div className="container">
-        <ToDoArea />
-        <Chat />
+        <div style={{ marginTop: "20px" }}>
+          <Chat />
+        </div>
       </div>
     );
   }
